7ff7ef9366e37056be7e9470e2e507492677141b
SHOrTEVA <37376947+SHOrTEVA@users.noreply.github.com>
2025-05-15 22:56:38 +0800

    Adjust layout

--- frontend/src/arkham/components/Investigator.vue
+++ frontend/src/arkham/components/Investigator.vue
@@ -557,6 +557,12 @@ i.action {
     height: min-content;
     align-self: center;
   }
+  @media (max-width: 800px) and (orientation: portrait)  {
+    justify-content: flex-end;
+    gap: 0;
+    width: fit-content;
+    align-items: flex-end;
+  }
 }
 
 .turn-info {
@@ -651,6 +657,12 @@ i.action {
   width: calc(var(--card-width) * var(--card-sideways-aspect));
   @media (max-width: 800px) and (orientation: portrait) {
     width: 65%;
+    display: flex;
+    flex-direction: row;
+    gap: 1px;
+    :deep(.card) {
+      height: calc(var(--card-width) * 3);
+    }
   }
 }
 
@@ -690,6 +702,12 @@ i.action {
 .stats {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
+  @media (max-width: 800px) and (orientation: portrait) {
+    display: flex;
+    flex-direction: column;
+    width:6.8vw;
+  }
+
 }
 
 .willpower {
@@ -758,7 +776,7 @@ i.action {
 
 .player-container{
   @media (max-width: 800px) and (orientation: portrait) {
-    width: 75%;
+    width: 100%;
   }
 }
 
@@ -777,6 +795,11 @@ i.action {
   }
   @media (max-width: 800px) and (orientation: portrait) {
     margin-left: auto;
+    flex-direction: row;
+    //height:  calc(var(--card-width) * 1.3);
+    :deep(span){
+      display: none;
+    }
   }
 }
 
