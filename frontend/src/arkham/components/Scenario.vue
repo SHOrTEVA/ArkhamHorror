3f20e04cd8d0dfdac59584664c453eb7fd956698
SHOrTEVA <37376947+SHOrTEVA@users.noreply.github.com>
2025-06-13 03:34:15 +0800

    Update Scenario.vue

--- frontend/src/arkham/components/Scenario.vue
+++ frontend/src/arkham/components/Scenario.vue
@@ -1017,6 +1017,7 @@ const showVictoryDisplay = () => doShowCards(victoryDisplay, t('scenario.victory
   scroll-padding: 30%;
   place-content: safe center;
   display: grid;
+  /*Non-mobile with the columns being stretched out extensively*/
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   flex-shrink: 0;
 }
