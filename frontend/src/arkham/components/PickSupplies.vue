7ceb5e3805730f94c47b98f9782902bacbacd86f
SHOrTEVA <37376947+SHOrTEVA@users.noreply.github.com>
2025-06-18 13:15:15 +0800

    fix: pick supplies page unusable

--- frontend/src/arkham/components/PickSupplies.vue
+++ frontend/src/arkham/components/PickSupplies.vue
@@ -3,6 +3,7 @@ import { computed } from 'vue';
 import { PickSupplies  } from '@/arkham/types/Question';
 import { MessageType } from '@/arkham/types/Message';
 import { imgsrc } from '@/arkham/helpers';
+import { useI18n } from 'vue-i18n';
 
 const props = defineProps<{
   game: Game,
@@ -11,7 +12,7 @@ const props = defineProps<{
 }>()
 const emit = defineEmits(['choose'])
 const choose = (idx: number) => emit('choose', idx)
-
+const { t } = useI18n()
 
 const investigatorId = computed(() => Object.values(props.game.investigators).find((i) => i.playerId === props.playerId)?.id)
 const pointsRemaining = computed(() => props.question.pointsRemaining)
@@ -36,40 +37,54 @@ const portrait = (investigatorId: string) => {
 
 </script>
 <template>
-  <img :src="portrait(investigatorId)" alt="Investigator Portrait" class="portrait" />
-  <div class="pick-supplies">
-    <h2>Pick Supplies ({{pointsRemaining}} points remaining)</h2>
+  <div id="pick-supplies">
+    <div class="pick-supplies-top">
+      <div class="pick-supplies-portrait">
+        <img :src="portrait(investigatorId)" alt="Investigator Portrait" class="portrait" />
+      </div>
+      <div class="pick-supplies-contents"><ul><li v-html="t('theForgottenAge.prologue.pickSupplies')"></li></ul></div>
+    </div>
+    <div class="pick-supplies">
+      <h2>Pick Supplies ({{pointsRemaining}} points remaining)</h2>
 
-    <div class="supply-choices">
-      <template v-for="(choice, index) in supplies" :key="index">
-        <template v-if="choice.tag === MessageType.TOOLTIP_LABEL">
-          <button @click="choose(index + 1)" v-tooltip="choice.tooltip">{{choice.label}}</button>
+      <div class="supply-choices">
+        <template v-for="(choice, index) in supplies" :key="index">
+          <template v-if="choice.tag === MessageType.TOOLTIP_LABEL">
+            <button @click="choose(index + 1)" v-tooltip="choice.tooltip">{{choice.label}}</button>
+          </template>
         </template>
-      </template>
+      </div>
+
+      <button class="done-button" @click="choose(0)">Done</button>
     </div>
 
-    <button class="done-button" @click="choose(0)">Done</button>
-  </div>
+    <div class="pick-supplies" v-if="chosenSupplies.size > 0">
+      <h2>Chosen Supplies</h2>
 
-  <div class="pick-supplies" v-if="chosenSupplies.size > 0">
-    <h2>Chosen Supplies</h2>
-
-    <ul>
-      <template v-for="([supply, count], index) in chosenSupplies" :key="index">
-        <li v-if="count > 1">{{supply}} ({{count}})</li>
-        <li v-else>{{supply}}</li>
-      </template>
-    </ul>
+      <ul>
+        <template v-for="([supply, count], index) in chosenSupplies" :key="index">
+          <li v-if="count > 1">{{supply}} ({{count}})</li>
+          <li v-else>{{supply}}</li>
+        </template>
+      </ul>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+#pick-supplies {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
 .supply-choices {
   display: grid;
-  grid-template-columns: repeat(4, minmax(150px, 1fr));
+  grid-template-columns: repeat(4, minmax(calc(25% - 20px), 1fr));
   row-gap: 20px;
-  @media (max-width: 800px) and (orientation: portrait){
-    grid-template-columns: repeat(3, minmax(50px, 1fr));
+  @media (max-width: 800px) and (orientation: portrait) {
+    grid-template-columns: repeat(3, minmax(calc(34% - 20px), 1fr));
   }
 }
 
@@ -86,6 +101,22 @@ button {
   &:hover {
     background-color: #311b3e;
   }
+  @media (max-width: 800px) and (orientation: portrait) {
+    font-size: smaller;
+    margin:0 5px;
+  }
+}
+
+.pick-supplies-container {
+  background: #DCD6D0;
+  box-shadow: inset 0 0 170px rgba(0,0,0,0.5), 1px 1px 3px rgba(0,0,0,0.6);
+  width: calc(100% - 20px);
+  margin: 0 auto;
+  gap: 10px;
+  p {
+    margin: 0;
+    padding: 0;
+  }
 }
 
 .pick-supplies {
@@ -93,10 +124,19 @@ button {
   box-shadow: inset 0 0 170px rgba(0,0,0,0.5), 1px 1px 3px rgba(0,0,0,0.6);
   width: calc(100% - 20px);
   margin: 0 auto;
-  margin-top: 20px;
+  border-radius: 10px;
+  gap: 10px;
   p {
     margin: 0;
     padding: 0;
+  }
+  button {
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
+  h2 {
+    margin-block: 10px;
+    font-weight: bold;
   }
 }
 
@@ -145,6 +185,83 @@ ul li {
 }
 
 .portrait {
-  width: 150px;
+  width: 25vw;
+  max-width: 200px;
+}
+
+.pick-supplies-contents {
+  display: flex;
+  align-items: center;
+  background: #DCD6D0;
+  border-radius: 10px;
+  box-shadow: inset 0 0 170px rgba(0, 0, 0, 0.5), 1px 1px 3px rgba(0, 0, 0, 0.6);
+}
+
+.pick-supplies-portrait {
+  align-self: center;
+  min-width: fit-content;
+
+  img {
+    border-radius: 10px;
+    box-shadow: inset 0 0 170px rgba(0, 0, 0, 0.5), 1px 1px 3px rgba(0, 0, 0, 0.6);
+  }
+}
+
+.pick-supplies-top {
+  display: flex;
+  gap: 10px;
+  border-radius: 10px;
+  margin-inline: 10px;
+  container-type: inline-size;
+  @container (width < 500px) {
+    .portrait {
+      object-fit: contain;
+      max-width: 10vw;
+      min-width: 10vw;
+      margin: 20px;
+    }
+  }
+  ul {
+    list-style-type: "\0059";
+    padding-inline: 20px;
+    li {
+      background: unset;
+      text-transform: none;
+      font-weight: normal;
+      padding-left: 10px;
+      margin-left: 10px;
+      margin-bottom: 5px;
+      text-align: left;
+
+      ul, :deep(ul) {
+        margin-block: 10px;
+        list-style-type: "\e91a";
+        li::marker {
+          font-family: "ArkhamIcons";
+        }
+      }
+
+      :deep(li) {
+        padding-left: 10px;
+        margin-bottom: 10px;
+      }
+    }
+
+    li::marker, :deep(li::marker) {
+      font-family: "ArkhamSlim";
+      color: var(--spooky-green-dark);
+      padding-left: 10px;
+      margin-left: 10px;
+    }
+  }
+  @media (max-width: 800px) and (orientation: portrait) {
+    flex-direction: column;
+    .portrait {
+      object-fit: contain;
+      max-width: 20vw;
+      min-width: 20vw;
+      margin: 0;
+    }
+  }
 }
 </style>
